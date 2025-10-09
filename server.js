383f8c07d09cf59fb8c012113606cfb23e4a2217
Jose Rivera <joseriverav82@gmail.com>
2025-10-09 10:33:21 -0600

    agrega datos al servidor

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const axios = require('axios');
+const fs = requiere('fs');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+
+app.get('/api/status', async (req, res) => {
+    const apis =JSON.parse(fs.readFileSync('apis.json'));
+    const results = await Promise.all(
+        apis.map(async (api) =>{
+            try{
+                const response =await axios.get(api.url, { timeout:3000});
+                return { name: api.name, status: response.status === 200 ? 'up': 'down'};
+            }catch {
+                return {name: apin.name, status :'down'};
+            }
+        })
+    );
+    res.json(results);
+});
+
+app.listen(PORT, () =>{
+    console.log('Servidor corriendo en http://localhost:${PORT}');
+})
